e3d3ae5fcc4da1986565e05535a2bf728fb9cd4b
parthnonigaba <85085655+parthnonigaba@users.noreply.github.com>
2022-03-17 17:27:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot(){
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded!');
@@ -26,8 +26,44 @@ function modelLoaded(){
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data_1 = "The First Predicion is " + prediction_1;
-    speak_data_2 = "and the second predicion is " + prediction_2;
+    speak_data_1 = "The First Prediction is " + prediction_1;
+    speak_data_2 = ",and the second prediction is " + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById('result_emotion_name').innerHTML = results[0].label;
+        document.getElementById('result_emotion_name2').innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "good"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "bad"){
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+        if(results[0].label == "okay"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[1].label == "good"){
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "bad"){
+            document.getElementById("update_emoji2").innerHTML = "&#128078;";
+        }
+        if(results[1].label == "okay"){
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        
+    }
 }
